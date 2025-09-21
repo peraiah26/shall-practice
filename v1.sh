75b5a460476bd38d6f9e8c62259bcd4732ac69ef
name <peraiahtellagorla92@gmail.com>
2025-09-21 17:38:47 +0530

    some message

--- v1.sh
+++ v1.sh
@@ -6,4 +6,4 @@ PERSON=siva
 echo "$PERSON1:: hi $PERSON2, how are you?"
 echo "$PERSON2:: hello $PERSON1, i am fine. how are you?"
 echo "$PERSON1:: what are you learning recently?"
-echo "$PERSON2:: i am learning shell scripting $PERSON1"
+echo "$PERSON2:: i am learning shell scripting $PERSON1" 
