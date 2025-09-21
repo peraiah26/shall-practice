7a956b8e6bbfe7d30a2385b99cf7b5815adf2579
name <peraiahtellagorla92@gmail.com>
2025-09-21 23:15:44 +0530

    hello

--- special-var.sh
+++ special-var.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
  echo "all variables passed to the script: $@"
  echo "all variables passed to the script: $*"
+ echo "script name: $0"
+ echo "present working directory: $pwd"
+ echo "who runing this: $USER"
+ echo "home directory of user: $HOME"
+ echo "PID of the script: $$
