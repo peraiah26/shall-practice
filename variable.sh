f52217c5b9a82e8d37c5215f13ea6a1c2634d04c
name <peraiahtellagorla92@gmail.com>
2025-09-21 17:16:53 +0530

    some message

---
+++ variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person=anji
+person=siva
+
+echo "$person1:: hi $person2, how are you?"
+echo "$person2:: hello $person1, i am fine. how are you?"
+echo "$person1:: what are you learning recently?"
+echo "$person2:: i am learning shell scripting $person1"
