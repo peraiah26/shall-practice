97ece6e2ca55777bd56837d5d8c7d48c5383dd6d
name <peraiahtellagorla92@gmail.com>
2025-09-22 20:43:53 +0530

    condition

--- 01-condition.sh
+++ 01-condition.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 NUMBER=$1
 
-if [ $NUMBER -lt 10 ] ; then
+if [ $NUMBER -lt 10 ]; then
     echo "given number $NUMBER is less than 10"
+
+elif [ $NUMBER -eq 10 ]; then
+     echo "given number $NUMBER is equal to 10"
+
 
 else
     echo "given number $NUMBER is greater than 10"
