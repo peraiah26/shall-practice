520bcabe3782857aaa6fc7d34c4ab7cce9a75301
name <peraiahtellagorla92@gmail.com>
2025-09-24 21:48:41 +0530

    conservation

--- p-variable.sh
+++ p-variable.sh
@@ -3,3 +3,6 @@
 echo "All variables passed through the script:$@"
 echo "All variables passed through the script:$*"
 echo "script name is:$0"
+echo "current working dirctory:$PWD"
+echo "who runs :$WHO"
+echo "home directory:$HOME"
