19694f6b41835354a5af8bc616f3affcf7ad417a
name <peraiahtellagorla92@gmail.com>
2025-09-24 21:42:10 +0530

    conservation

--- p-variable.sh
+++ p-variable.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
-
-sleep 10
-
-END_TIME=$(date +%s)
-
-TOTAL_TIME=$(($END_TIME-$START_TIME))
-
-echo "script execution time is:$TOTAL_TIME"
+echo "All variables passed through the script:$@"
+echo "All variables passed through the script:$*"
