9fe58394e9ce297e244ab95a0d074230e8bf8316
name <peraiahtellagorla92@gmail.com>
2025-09-24 20:59:54 +0530

    conservation

--- p-variable.sh
+++ p-variable.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-person1=$1
-person2=$2
-
-echo "$person1:: hi $person2 how are you?"
-echo "$person2:: hello $person1 i am fine how are you?"
-echo "$person1:: what are you learning recently?"
-echo "$person2:: i am learning shell scripting $person1"
+echo "enter your pin number"
+read PIN-NUMBER
+echo "your pin number is:"
