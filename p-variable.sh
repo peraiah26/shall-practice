e00feba8c4e44145dfcd12aecb838dfe6625c444
name <peraiahtellagorla92@gmail.com>
2025-09-24 20:48:21 +0530

    conservation

--- p-variable.sh
+++ p-variable.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-person1=peraiah
-person2=ruturaj
+person1=$
+person2=$
 
 echo "$person1:: hi $person2 how are you?"
-echo "$person2:: hello $peraiah i am fine how are you?"
+echo "$person2:: hello $person1 i am fine how are you?"
 echo "$person1:: what are you learning recently?"
 echo "$person2:: i am learning shell scripting $person1"
