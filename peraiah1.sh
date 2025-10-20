71fff64bfbde3b8f0b96a8b7185a29f91d5bc044
name <peraiahtellagorla92@gmail.com>
2025-10-20 12:57:57 +0530

    conservation

---
+++ peraiah1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "peraiah::hi srinu how are you?"
+echo "srinu::hello peraiah i am fine how are you?"
+echo "peraiah::what are you learning recently?"
+echo "srinu::i am learning shell scripting peraiah"
