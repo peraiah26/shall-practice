1fa09df52d2acff44f54ceb995e761faf8cf35eb
name <peraiahtellagorla92@gmail.com>
2025-10-21 15:27:25 +0530

    conservation

---
+++ peraiah3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1::hi $PERSON2 how are you?"
+echo "$PERSON2::hello $PERSON1 i am fine how are you?"
+echo "$PERSON1::what are you learning recently?"
+echo "$PERSON2::i am learning shell scripting $PERSON1"
