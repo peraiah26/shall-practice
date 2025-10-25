0411d4df9a179d81010c9355ed2b1c70dbc21a3a
name <peraiahtellagorla92@gmail.com>
2025-10-25 12:40:26 +0530

    conversation

---
+++ pv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=peraiah
+person2=ganesh
+
+echo "person1: hii $person2 how are you"
+echo "person2: hello i am good $person1 how are you"
+echo "person1: tell me something yar"
+echo "person2: ntg bro you want to tell anything"
