e45942fe47bc56bbc30903cfa5652194ae7ca27f
name <peraiahtellagorla92@gmail.com>
2025-10-25 13:24:30 +0530

    conversation

--- pv.sh
+++ pv.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 
-START_TIME=($date +%s)
+#DATE=$(date)
 
-sleep 5
+START_TIME=$(date +%s)
 
-END_TIME=($date +%s)
+sleep 10
+
+END_TIME=$(date +%s)
 
 TOTAL_TIME=$(($END_TIME-$START_TIME))
 
-echo "total time second is:$TOTAL_TIME"
+echo "Script executed in: $TOTAL_TIME Seconds"
+
