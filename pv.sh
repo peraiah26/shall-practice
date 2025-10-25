b658f6a50488cf46650c90935cee9f69d25b596d
name <peraiahtellagorla92@gmail.com>
2025-10-25 13:32:18 +0530

    conversation

--- pv.sh
+++ pv.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 
-echo "All variables passed to the script: $@"
-echo "All variables passed to the script: $*"
-echo "Script name: $0"
-echo "Current directory: $PWD"
-echo "Who is running this: $USER"
-echo "Home directory of user: $HOME"
-echo "PID of this script: $$"
-sleep 50 &
-echo "PID of the last command in background is: $!"
+a=10
+b=24
+
+sum=$(($a+$b))
+
+echo "addition of two numbers is:${sum}"
