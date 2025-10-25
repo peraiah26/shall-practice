4dcf11d80254894328f2effe699de5c75f5678ee
name <peraiahtellagorla92@gmail.com>
2025-10-25 13:34:01 +0530

    conversation

--- pv.sh
+++ pv.sh
@@ -7,3 +7,9 @@ b=24
 sum=$(($a+$b))
 
 echo "addition of two numbers is:${sum}"
+
+LEADERS=("Modi" "Putin" "Trudo" "Trump")
+
+echo "All leaders: ${LEADERS[@]}"
+echo "First Leader: ${LEADERS[0]}"
+echo "First Leader: ${LEADERS[10]}"
